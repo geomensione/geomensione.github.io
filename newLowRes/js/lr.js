fd6dea63c5565733125d421f2b6e9f0977a24602
geomensione <michele.diegoli@outlook.it>
2017-01-14 13:03:39 +0100

    Update lr.js

--- newLowRes/js/lr.js
+++ newLowRes/js/lr.js
@@ -219,7 +219,7 @@ function setCanvas(){
 	lr['resolution'] = 48;
 	lr['framerate'] = 30;
 	lr['fallingitem'] = [];
-	lr['size'] = 0;
+	lr['size'] = 15;
 	lr['fortune'] = 10;
 	lr['time'] = 0;
 	lr['res_sprites'] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
