1c571244a5756c0bd50a237a1e4760d9d9af7ee6
geomensione <michele.diegoli@outlook.it>
2017-01-15 00:06:30 +0100

    Create keys.js

---
+++ newLowRes/js/keys.js
@@ -0,0 +1,35 @@
+function move_hero(e){
+	switch(e.keyCode) {
+		case 37: //leftarrow
+			goLeft = true;
+			//go_left();				
+			break;
+		case 38:
+			jump = true;
+			break;
+	    case 39: //rightarrow
+			goRight=true;
+			//go_right();
+			break;
+		default:
+			console.log(e.keyCode);//38 arrowup, 40 arrow down
+	}
+}
+
+function key_up(e){
+	switch(e.keyCode) {
+		case 37: //leftarrow
+			goLeft = false;
+			//go_left();				
+			break;
+		case 38:
+			jump = false;
+			break;
+	    case 39: //rightarrow
+			goRight=false;
+			//go_right();
+			break;
+		default:
+			console.log(e.keyCode);//38 arrowup, 40 arrow down
+	}
+}
