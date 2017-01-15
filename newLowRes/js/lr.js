e71ebdf34bc5e9a42fe3e2dabfb4c03f53dcf097
geomensione <michele.diegoli@outlook.it>
2017-01-15 09:23:07 +0100

    Update lr.js

--- newLowRes/js/lr.js
+++ newLowRes/js/lr.js
@@ -136,41 +136,9 @@ function update(){
 	
 }
 
-function go_left(){
-	if((lr['heroposx']<=0) === false){
-		lr['heroposx']-=1;
-	}
-	
-}
-
-function go_right(){
-	if(((lr['heroposx']+1)>(lr['rx']-h.size)) === false){
-				lr['heroposx']+=1;
-			}
-}
-
-function reset_event(){
-	goLeft = goRight = false;
-}
 
 
-function l_r_hero(e){
-	if(goLeft)
-		go_left();				
-	if(goRight)
-		go_right();
-}
 
-function start_hero(e){
-	switch(e.keyCode) {
-		case 83: //s
-			clearInterval(cicle);
-			setCanvas();				
-			break;
-		default:
-			console.log(e.keyCode);//38 arrowup, 40 arrow down
-	}
-}
 
 function audio(){
 }
