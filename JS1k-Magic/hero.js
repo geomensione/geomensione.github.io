feb7740af95aa9c6ddb484f45c0616c2de17edc8
geomensione <michele.diegoli@outlook.it>
2017-01-31 20:11:16 +0100

    Create hero.js

---
+++ JS1k-Magic/hero.js
@@ -0,0 +1,48 @@
+"use strict";
+
+function go_left(){
+	if((lr['heroposx']<=0) === false){
+		lr['heroposx']-=1;
+	}
+	
+}
+
+function go_right(){
+	if(((lr['heroposx']+1)>(lr['rx']-h.size)) === false){
+				lr['heroposx']+=1;
+			}
+}
+
+function go_jump(){
+	if(lr['jump']>0 && lr['descent'] === false){
+		lr['heroposy']-=1;
+		lr['jump']-=1;
+	}else{
+		if(lr['descent'] === false){
+			lr['descent'] = true;
+		}
+		if(lr['jump']<16){
+			lr['heroposy']+=1;
+			lr['jump']+=1;
+		}else{
+			lr['descent'] = false;
+			jump = false;
+		}
+
+		
+	}
+}
+
+function reset_event(){
+	goLeft = goRight = false;
+}
+
+
+function l_r_hero(e){
+	if(goLeft)
+		go_left();				
+	if(goRight)
+		go_right();
+	if(jump)
+		go_jump();
+}
