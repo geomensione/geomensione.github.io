45d33eb5b6f8199aa2bd04ca70c1b3f516ef20d9
geomensione <michele.diegoli@outlook.it>
2017-01-31 20:13:04 +0100

    Create keys.js

---
+++ JS1k-Magic/keys.js
@@ -0,0 +1,50 @@
+"use strict";
+
+function move_hero(e){
+	switch(e.keyCode) {
+		case 37: //leftarrow
+			goLeft = true;
+			lr['direction'] = 'L';			
+			break;
+		case 38:
+			jump = true;
+			break;
+	    case 39: //rightarrow
+			goRight=true;
+			lr['direction'] = 'R';
+			break;
+		default:
+			console.log(e.keyCode);//38 arrowup, 40 arrow down
+	}
+}
+
+function key_up(e){
+	switch(e.keyCode) {
+		case 37: //leftarrow
+			goLeft = false;
+			//go_left();				
+			break;
+			/*
+		case 38:
+			jump = false;
+			break;
+			*/
+	    case 39: //rightarrow
+			goRight=false;
+			//go_right();
+			break;
+		default:
+			console.log(e.keyCode);//38 arrowup, 40 arrow down
+	}
+}
+
+function start_hero(e){
+	switch(e.keyCode) {
+		case 83: //s
+			clearInterval(cicle);
+			setCanvas();				
+			break;
+		default:
+			console.log(e.keyCode);//38 arrowup, 40 arrow down
+	}
+}
