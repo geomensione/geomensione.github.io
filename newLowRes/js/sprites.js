6bc4de7ef2592ef96e720d566165329092953e9c
geomensione <michele.diegoli@outlook.it>
2017-02-13 13:34:13 +0100

    Update sprites.js

--- newLowRes/js/sprites.js
+++ newLowRes/js/sprites.js
@@ -518,7 +518,7 @@ function hero(){
 	this.draw = function(){
 		//this.drawSprite('doc_boy', lr['ry']-lr['res_sprites'][lr['size']], lr['heroposx']);
 		this.drawSprite('shark_hunter_'+lr['direction']+'_'+(this.frame%2), lr['heroposy'], lr['heroposx']);
-		if(jump && !fall)
+		if(!jump && !fall)
 			this.frame = (this.frame === 1)?0:1;
 	};
 	this.create = function(){
