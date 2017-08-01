51e7e73a08da92f0e3e30da1d928c0f40d1a529c
geomensione <michele.diegoli@outlook.it>
2017-08-01 14:29:09 +0200

    Update f3d.js

--- threejs/js/f3d.js
+++ threejs/js/f3d.js
@@ -21,6 +21,14 @@ var f3d = function(){
 	let lastSphereCenterY;
 	let oldX,oldY;
 	let lastSphere;
+	var container;
+	var camera, scene, renderer, rollOverGeo;
+	var rollOverMesh, rollOverMaterial;
+	var cubeGeo, cubeMaterial;
+	var mouse, raycaster, isShiftDown = false;
+	var objects = [];
+	var plane, cube;
+			
 	function Sphere(){
 		var geometry = new THREE.SphereGeometry( 5, 32, 32 );
 		var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
@@ -33,6 +41,74 @@ var f3d = function(){
 
 		return Math.sqrt( a*a + b*b );
 	}
+	function init() {
+		container = document.createElement( 'div' );
+		document.body.appendChild( container );
+		var info = document.createElement( 'div' );
+		info.style.position = 'absolute';
+		info.style.top = '10px';
+		info.style.width = '100%';
+		info.style.textAlign = 'center';
+		info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
+		container.appendChild( info );
+		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+		camera.position.set(0, 1000, 0 );
+		camera.lookAt( new THREE.Vector3() );
+		scene = new THREE.Scene();
+		// roll-over helpers
+		rollOverGeo = new THREE.BoxGeometry( 50, 50, 50 );
+		rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+		rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
+		//scene.add( rollOverMesh );
+		// cubes
+		cubeGeo = new THREE.BoxGeometry( 2, 2, 2 );
+		cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, map: new THREE.TextureLoader().load( "textures/square-outline-textured.png" ) } );
+		// grid
+		var size = 500, step = 10;
+		var geometry = new THREE.Geometry();
+		for ( var i = - size; i <= size; i += step ) {
+			geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+			geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+			geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+			geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+		}
+		var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+		var line = new THREE.LineSegments( geometry, material );
+		scene.add( line );
+		//
+		raycaster = new THREE.Raycaster();
+		mouse = new THREE.Vector2();
+		var geometry = new THREE.PlaneBufferGeometry( 1000, 1000 );
+		geometry.rotateX( - Math.PI / 2 );
+		plane = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
+		scene.add( plane );
+		objects.push( plane );
+		// Lights
+		var ambientLight = new THREE.AmbientLight( 0x606060 );
+		scene.add( ambientLight );
+		var directionalLight = new THREE.DirectionalLight( 0xffffff );
+		directionalLight.position.set( 1, 0.75, 0.5 ).normalize();
+		scene.add( directionalLight );
+		renderer = new THREE.WebGLRenderer( { antialias: true } );
+		renderer.setClearColor( 0xf0f0f0 );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( renderer.domElement );
+		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+		document.addEventListener( 'touchmove', onDocumentMobileMouseMove, false );
+		document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+		document.addEventListener( 'touchstart', onDocumentMobileMouseDown, false );
+		document.addEventListener( 'keydown', onDocumentKeyDown, false );
+		document.addEventListener( 'keyup', onDocumentKeyUp, false );
+		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+		document.addEventListener( 'touchend', onDocumentMobileMouseUp, false );
+		//
+		window.addEventListener( 'resize', onWindowResize, false );
+	}
+	function render() {
+		renderer.render( scene, camera );
+	}
+
 	return{
 		setOldCoord: function(x,y){
 			oldX = x;
@@ -58,14 +134,16 @@ var f3d = function(){
 			lastSphere.scale.x = scale;
 			lastSphere.scale.y = scale;
 			lastSphere.scale.z = scale;
-		}
+		},
+		init: init,
+		render: render
 	}
 }
 
 var f = new f3d();
-			
-			init();
-			render();
+f.init();
+f.render();
+
 function init() {
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
@@ -532,7 +610,4 @@ function onDocumentKeyUp( event ) {
 	switch ( event.keyCode ) {
 		case 16: isShiftDown = false; break;
 	}
-}
-function render() {
-	renderer.render( scene, camera );
 }
