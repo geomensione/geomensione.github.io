9178a8ae22c395481451a48d5b5a26e472fa5d57
geomensione <michele.diegoli@outlook.it>
2017-08-01 17:22:12 +0200

    Update f3d.js

--- threejs/js/f3d.js
+++ threejs/js/f3d.js
@@ -1,26 +1,10 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-			var container;
-			var camera, scene, renderer;
-			var plane, cube;
-			var mouse, raycaster, isShiftDown = false;
-			var rollOverMesh, rollOverMaterial;
-			var cubeGeo, cubeMaterial;
 			
-			var gest = new Array();
-			var draw_mode = false;
-			//var r = new DollarRecognizer();
-			var maxX, minX, maxY, minY, _3dmaxX, _3dminX, _3dmaxZ, _3dminZ = 0;
-			var objects = [];
-			var mystroke = new Array();
-      		var draw = [];
-			var circle_in_scene = 0;
-			
-			var points = [];
 var f3d = function(){
-	let lastSphereCenterX;
-	let lastSphereCenterY;
-	let oldX,oldY;
-	let lastSphere;
+	var lastSphereCenterX;
+	var lastSphereCenterY;
+	var oldX,oldY;
+	var lastSphere;
 	var container;
 	var camera, scene, renderer, rollOverGeo;
 	var rollOverMesh, rollOverMaterial;
