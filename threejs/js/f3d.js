fdd18202fecc9e79185f26a04ac47f38b9312686
geomensione <michele.diegoli@outlook.it>
2017-08-02 09:33:36 +0200

    Update f3d.js

--- threejs/js/f3d.js
+++ threejs/js/f3d.js
@@ -489,9 +489,9 @@ var f3d = function(){
 		if (min_r === 0)
 			min_r = 1;
 		let scale = max_r/min_r;
-		this.lastSphere.scale.x = scale;
-		this.lastSphere.scale.y = scale;
-		this.lastSphere.scale.z = scale;
+		lastSphere.scale.x = scale;
+		lastSphere.scale.y = scale;
+		lastSphere.scale.z = scale;
 	}
 	return{
 		getOldCoord: function(){
