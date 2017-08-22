f97d9ca6a9cf3e441193d328fbc47e6b9a92c9fc
geomensione <michele.diegoli@outlook.it>
2017-08-22 12:28:27 +0200

    Update game.js

--- JS13KGames-Lost/game.js
+++ JS13KGames-Lost/game.js
@@ -14,7 +14,8 @@ JS1K start
 var la = [], 
     fr = 100, 
     g = {}, 
-    b = {}, 
+    b = {},
+    bo = {},
     l_i,
     sh = [
         'W0W00000',
@@ -141,11 +142,82 @@ b.d_fn = function(){
         cx += o;
         cy = 0;
     }
+}
+
+bo.w = window.innerWidth;
+bo.h = window.innerHeight;
+//resolution
+bo.r = 96;
+//sprite dimension
+bo.s = 1;
+//fill fb only one time
+bo.do = 0;
+//number of stars
+bo.sts = 50;
+
+//start point when move background
+b.mx = 0;
+b.my = 0;
+bo.fb_fn = function(){
+    if(!this.do){
+        this.do = 1;
+        
+        this.fb = [];
+        let rx = this.rx,
+            ry = this.ry;
+        for(let i = 0;i<rx;i++){
+            this.fb[i] = [];
+            for(let d = 0;d<ry;d++){
+                //clear all
+                this.fb[i][d] = ['00', '00', '00'];
+            }
+        }
+        //every time is a different stars landscape
+        for(let s = 0;s<this.sts;s++){
+            let x = Math.floor(Math.random() * (this.rx-this.s));
+            let y = Math.floor(Math.random() * (this.ry-this.s));
+            this.fb[x][y] = ['00','00','AA'];       
+        }
+    }
+    
+}
+
+bo.d_fn = function(){
+    let rx = this.rx,
+        ry = this.ry,
+        o = this.o,
+        cx = 0, 
+        cy = 0,
+        x_l = rx,
+        y_l = ry;
+    for(let i = this.mx,l = 0;l<rx;l++){
+        for(let d = this.my,ll=0;ll<ry;ll++){
+            if(this.fb[i][d][0] !== '00'){
+                c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
+                c.fillRect(cx,cy,o,o);
+            }
+            if((d+1)<ry)
+                d += 1;
+            else
+                d = 0;
+            cy += o;
+        }
+        if((i+1)<rx)
+            i += 1;
+        else
+            i = 0;
+        cx += o;
+        cy = 0;
+    }
 }    
+
+
+//bottom
+la[0]=bo;
 //background
-la[0]=b;
+la[1]=b;
 //frames
-la[1]=g;
+la[2]=g;
 d.onkeydown = function(e){
     if((la[0].mx+1)<la[0].rx)
         la[0].mx += 1;
