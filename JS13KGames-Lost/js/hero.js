bfac7e667da967710e8e83c0369d13ac1a4471df
geomensione <michele.diegoli@outlook.it>
2017-08-24 14:04:12 +0200

    Create hero.js

---
+++ JS13KGames-Lost/js/hero.js
@@ -0,0 +1,43 @@
+//start text layer
+h.w = window.innerWidth;
+h.h = window.innerHeight;
+//resolution
+h.r = 36;
+//sprite dimension
+h.s = 5;
+//score
+h.sc = '';
+h.fb_fn = function(){
+    this.fb = [];
+    let rx = this.rx,
+        ry = this.ry;
+    for(let i = 0;i<rx;i++){
+        this.fb[i] = [];
+        for(let d = 0;d<ry;d++){
+            //clear all
+            this.fb[i][d] = ['00', '00', '00'];
+        }
+    }
+    this.sc = qx+' '+qy;
+    drawText(this);
+}
+
+h.d_fn = function(){
+    let rx = this.rx,
+        ry = this.ry,
+        o = this.o,
+        cx = 0, 
+        cy = 0;
+    for(let i = 0;i<rx;i++){
+        for(let d = 0;d<ry;d++){
+            if(this.fb[i][d][0] !== '00'){
+                c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
+                c.fillRect(cx,cy,o,o);
+            }
+            cy += o;
+        }
+        cx += o;
+        cy = 0;
+    }
+}    
+//end text layer
