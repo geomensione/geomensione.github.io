d87b1ba05a71ff9f708d4ea6496caba064e5a6d5
geomensione <michele.diegoli@outlook.it>
2017-09-01 14:04:58 +0200

    Update hero.js

--- JS13KGames-Lost/js/hero.js
+++ JS13KGames-Lost/js/hero.js
@@ -65,10 +65,14 @@ h.d_fn = function(){
     if(checkQuadrant(qx,qy)){
         for(let i = 0;i<sx;i++){
             for(let d = 0;d<sy;d++){
+                /*
                 if(this.fb[i][d][0] !== '00'){
                     c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
                     c.fillRect(cx,cy,o,o);
                 }
+                */
+                c.fillStyle = "rgba(0,255,0,0.5)";
+                c.fillRect(cx,cy,o,o);
                 cy += o;
             }
             cx += o;
