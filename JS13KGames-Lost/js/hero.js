19fbf1ecc83456750800c96e8d5d669e975afcd5
geomensione <michele.diegoli@outlook.it>
2017-09-05 13:49:41 +0200

    Update hero.js

--- JS13KGames-Lost/js/hero.js
+++ JS13KGames-Lost/js/hero.js
@@ -51,6 +51,19 @@ h.fb_fn = function(){
     
 }
 
+h.hit = function(x,y,width,height,fn){
+    var imgd = context.getImageData(x, y, width, height);
+    var pix = imgd.data;
+
+    // Loop over each pixel and invert the color.
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        pix[i  ] = 255 - pix[i  ]; // red
+        pix[i+1] = 255 - pix[i+1]; // green
+        pix[i+2] = 255 - pix[i+2]; // blue
+        // i+3 is alpha (the fourth element)
+    }
+}
+
 h.d_fn = function(){
     let rx = this.rx,
         ry = this.ry,
