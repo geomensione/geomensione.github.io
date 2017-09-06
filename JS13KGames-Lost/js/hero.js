c581252a0db3ef93b9a97d6040dc7229e0815af7
geomensione <michele.diegoli@outlook.it>
2017-09-06 13:56:48 +0200

    Update hero.js

--- JS13KGames-Lost/js/hero.js
+++ JS13KGames-Lost/js/hero.js
@@ -97,8 +97,8 @@ h.d_fn = function(){
         condition_d = ry;
 
         if(checkQuadrant(this,qx,qy)){
-            i_o=rx-sx;
-            d_o=ry-sy;
+            i_o=rx-sx-1;
+            d_o=ry-sy-1;
         }
 
         //check if I'am in up quadrant
