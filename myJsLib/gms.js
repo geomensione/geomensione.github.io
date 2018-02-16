a7059d06f6390ac7485697bca891c88029838e1a
geomensione <michele.diegoli@outlook.it>
2018-02-16 15:22:07 +0100

    Update gms.js

--- myJsLib/gms.js
+++ myJsLib/gms.js
@@ -1,5 +1,13 @@
 gms = {};
 gms.prototype.d = document;
+
+gms.prototype.consoleObject = (o) => {
+  console.log(JSON.stringify(o));
+}
+  
+gms.prototype.printObject = (o) => {
+  document.writeln(JSON.stringify(o));
+}
 //do nothing
 gms.prototype.fuzzy = () => {};
 //get element from class name
@@ -20,7 +28,22 @@ gms.prototype.removeClass = (el,cls) => {
   el.classList.contains(cls))?el.classList.remove(cls):fuzzy();
 };
 //add css class
-gms.addClass = (el,cls) => {
+gms.prototype.addClass = (el,cls) => {
   el.classList.add(cls);
 };
-
+//shuffle fn
+gms.prototype.shuffle = (array) => {
+    let counter = array.length;
+    // While there are elements in the array
+    while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+      // Decrease counter by 1
+      counter--;
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+  }
