f4c360b974827e6b5956c8a1385d0fd34282ad41
geomensione <michele.diegoli@outlook.it>
2018-02-16 17:17:54 +0100

    Update gms.js

--- myJsLib/gms.js
+++ myJsLib/gms.js
@@ -1,49 +1,56 @@
-gms = {};
-gms.prototype.d = document;
+gms = (function(){
+  var gms = {
+      d: document,
 
-gms.prototype.consoleObject = (o) => {
-  console.log(JSON.stringify(o));
-}
-  
-gms.prototype.printObject = (o) => {
-  document.writeln(JSON.stringify(o));
-}
-//do nothing
-gms.prototype.fuzzy = () => {};
-//get element from class name
-gms.prototype.getFromClass = (str,i) => {
-  if(i)
-    return this.d.getElementsByClassName(str)[i];
-  else
-    return this.d.getElementsByClassName(str)[0];
-}
-//get element from class name
-gms.prototype.getAllFromClass = (str,i) => {
-  return this.d.getElementsByClassName(str);
-}
-//remove css class name
-gms.prototype.removeClass = (el,cls) => {
-  if(typeof el === 'string')
-    el = this.getFromClass(el);
-  el.classList.contains(cls)?el.classList.remove(cls):fuzzy();
-};
-//add css class
-gms.prototype.addClass = (el,cls) => {
-  el.classList.add(cls);
-};
-//shuffle fn
-gms.prototype.shuffle = (array) => {
-    let counter = array.length;
-    // While there are elements in the array
-    while (counter > 0) {
-      // Pick a random index
-      let index = Math.floor(Math.random() * counter);
-      // Decrease counter by 1
-      counter--;
-      // And swap the last element with it
-      let temp = array[counter];
-      array[counter] = array[index];
-      array[index] = temp;
-    }
-    return array;
+      consoleObject: (o) => {
+        console.log(JSON.stringify(o));
+      },
+
+      printObject: (o) => {
+        document.writeln(JSON.stringify(o));
+      },
+      //do nothing
+      fuzzy: () => {},
+      //get element from class name
+      getFromClass: (str,i) => {
+        if(i)
+          return this.d.getElementsByClassName(str)[i];
+        else
+          return this.d.getElementsByClassName(str)[0];
+      },
+      //get element from class name
+      getAllFromClass: (str,i) => {
+        return this.d.getElementsByClassName(str);
+      },
+      //remove css class name
+      removeClass: (el,cls) => {
+        if(typeof el === 'string')
+          el = this.getFromClass(el);
+        el.classList.contains(cls)?el.classList.remove(cls):fuzzy();
+      },
+      //add css class
+      addClass: (el,cls) => {
+        el.classList.add(cls);
+      },
+      //shuffle fn
+      shuffle: (array) => {
+          let counter = array.length;
+          // While there are elements in the array
+          while (counter > 0) {
+            // Pick a random index
+            let index = Math.floor(Math.random() * counter);
+            // Decrease counter by 1
+            counter--;
+            // And swap the last element with it
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+          }
+          return array;
+      }
+   
   }
+
+  return gms;
+
+}());
