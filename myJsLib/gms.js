55e9507b96a9a2428b00c01d0752dc7a30f8f7a2
geomensione <michele.diegoli@outlook.it>
2018-02-16 15:09:13 +0100

    Update gms.js

--- myJsLib/gms.js
+++ myJsLib/gms.js
@@ -3,8 +3,15 @@ gms.prototype.d = document;
 //do nothing
 gms.prototype.fuzzy = () => {};
 //get element from class name
-gms.prototype.getFromClass = (str) => {
-  return this.d.getElementsByClassName(str)[0];
+gms.prototype.getFromClass = (str,i) => {
+  if(i)
+    return this.d.getElementsByClassName(str)[i];
+  else
+    return this.d.getElementsByClassName(str)[0];
+}
+//get element from class name
+gms.prototype.getAllFromClass = (str,i) => {
+  return this.d.getElementsByClassName(str);
 }
 //remove css class name
 gms.prototype.removeClass = (el,cls) => {
