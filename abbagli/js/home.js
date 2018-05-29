0febb1769f456b217f52b7aeb6b3406feead4e52
Morticcino <michele.diegoli@outlook.it>
2018-05-29 20:49:19 +0200

    Update home.js

--- abbagli/js/home.js
+++ abbagli/js/home.js
@@ -2,8 +2,15 @@ function loadSvg(str){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-		   // Typical action to be performed when the document is ready:
+		   var ratio = 41.216/54.886;
 		   document.body.innerHTML = xhttp.responseText;
+			if(window.innerWidth>window.innerHeight){
+				document.svg.height = window.innerHeight;
+				document.svg.width = window.innerHeight/ratio
+			}else{
+				document.svg.width = window.innerWidth;
+				document.svg.height = window.innerWidth*ratio;
+			}
 		}
 	};
 	xhttp.open("GET", "img/"+str, true);
