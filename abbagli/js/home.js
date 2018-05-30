75b4721e5847cb36c77ffa4326a96ffdef212647
Morticcino <michele.diegoli@outlook.it>
2018-05-30 10:02:30 +0200

    Update home.js

--- abbagli/js/home.js
+++ abbagli/js/home.js
@@ -2,9 +2,9 @@ function loadSvg(str){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-		   var ratio = 41.216/54.886;
-		   var sezioni = ['home','colane','contatti','bracciali','anelli'];
-		   document.body.innerHTML = xhttp.responseText;
+			var ratio = 41.216/54.886;
+			var sezioni = ['home','colane','contatti','bracciali','anelli'];
+			document.body.innerHTML = xhttp.responseText;
 			var svg = document.getElementsByTagName('svg')[0];
 			if(window.innerWidth>window.innerHeight){
 				svg.setAttribute('height', window.innerHeight);
@@ -13,6 +13,13 @@ function loadSvg(str){
 				svg.setAttribute('width', window.innerWidth);
 				svg.setAttribute('height', window.innerWidth/ratio);
 			}
+			
+			var ellipse_sections = document.getElementsByTagName('ellipse');
+			var section_index = 0;
+			ellipse_sections.forEach((e)=>{
+				e.setAttribute('id',sezioni[section_index]);
+				++section_index;
+			});
 			
 		}
 	};
