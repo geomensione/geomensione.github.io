1d18f561638b954e4fdb83662eb3e5d8d3b5439d
Morticcino <michele.diegoli@outlook.it>
2018-06-05 13:37:39 +0200

    Update home.js

--- abbagli/js/home.js
+++ abbagli/js/home.js
@@ -31,11 +31,9 @@ function LightenDarkenColor(col, amt) {
 
 function sectionEvent(e){
 	var str_page = (typeof e === 'string')? e : e.id;
-	if(str_page.indexOf('home') === -1){
-		localStorage.page = str_page;
-		document.location = 'page.html';
-	}
-	
+	localStorage.page = str_page;
+	document.location = 'page.html';
+
 }
 function sectionEnter(e){
 	var color = e.getAttribute('fill');
