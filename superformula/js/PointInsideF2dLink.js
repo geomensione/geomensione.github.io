e53cabdb4c078b5faaf7c82624975808a0d022a7
Morticcino <michele.diegoli@outlook.it>
2018-07-13 12:46:11 +0200

    Create PointInsideF2dLink.js

---
+++ superformula/js/PointInsideF2dLink.js
@@ -0,0 +1,16 @@
+function (point, vs) {
+    
+    var x = point[0], y = point[1];
+    
+    inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+};
