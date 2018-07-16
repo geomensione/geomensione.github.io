a2d50723b7435c6967aaff2a7fd5c2c03e43a47b
Morticcino <michele.diegoli@outlook.it>
2018-07-16 16:28:53 +0200

    Update PointInsideF2dLink.js

--- superformula/js/PointInsideF2dLink.js
+++ superformula/js/PointInsideF2dLink.js
@@ -36,6 +36,59 @@ function segment_intersection(x1,y1,x2,y2, x3,y3,x4,y4) {
     return {x: x, y: y};
 }
 
+function doCircleLineInt(cx,cy,r,x2,y2){
+    var center_point = {
+        x: cx,
+        y: cy,
+    }
+    var endLine = {
+        x: x2,
+        y: y2,
+    }
+    var circle = {
+        radius: r,
+        center: center_point
+    }
+    var line = {
+        p1: center_point,
+        p2: endLine
+    }
+    return inteceptCircleLineSeg(circle,line);
+}
+
+function inteceptCircleLineSeg(circle, line){
+    var a, b, c, d, u1, u2, ret, retP1, retP2, v1, v2;
+    v1 = {};
+    v2 = {};
+    v1.x = line.p2.x - line.p1.x;
+    v1.y = line.p2.y - line.p1.y;
+    v2.x = line.p1.x - circle.center.x;
+    v2.y = line.p1.y - circle.center.y;
+    b = (v1.x * v2.x + v1.y * v2.y);
+    c = 2 * (v1.x * v1.x + v1.y * v1.y);
+    b *= -2;
+    d = Math.sqrt(b * b - 2 * c * (v2.x * v2.x + v2.y * v2.y - circle.radius * circle.radius));
+    if(isNaN(d)){ // no intercept
+        return [];
+    }
+    u1 = (b - d) / c;  // these represent the unit distance of point one and two on the line
+    u2 = (b + d) / c;    
+    retP1 = {};   // return points
+    retP2 = {}  
+    ret = []; // return array
+    if(u1 <= 1 && u1 >= 0){  // add point if on the line segment
+        retP1.x = line.p1.x + v1.x * u1;
+        retP1.y = line.p1.y + v1.y * u1;
+        ret[0] = retP1;
+    }
+    if(u2 <= 1 && u2 >= 0){  // second add point if on the line segment
+        retP2.x = line.p1.x + v1.x * u2;
+        retP2.y = line.p1.y + v1.y * u2;
+        ret[ret.length] = retP2;
+    }       
+    return ret;
+}
+
 function isInLink(point, vs) {
     
     var x = point[0], y = point[1];
