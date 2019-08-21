7b09cf37590b8c0933e2fd813587404f8141cfad
Michele <morticcino@diegoli.org>
2019-08-21 16:08:18 +0200

    Update bottom.js

--- JS13KGames-Back/js/bottom.js
+++ JS13KGames-Back/js/bottom.js
@@ -16,26 +16,33 @@ bo.sp = 1;
 bo.mx = 0;
 bo.my = 0;
 bo.fb_fn = function(){
-    if(!this.do){
-        this.do = 1;
-        
-        this.fb = [];
-        let rx = this.rx,
-            ry = this.ry;
-        for(let i = 0;i<rx;i++){
-            this.fb[i] = [];
-            for(let d = 0;d<ry;d++){
-                //clear all
-                this.fb[i][d] = ['00', '00', '00'];
+    return new Promise(function(res,rej){
+        if(!this.do){
+            this.do = 1;
+
+            this.fb = [];
+            let rx = this.rx,
+                ry = this.ry;
+            for(let i = 0;i<rx;i++){
+                this.fb[i] = [];
+                for(let d = 0;d<ry;d++){
+                    //clear all
+                    this.fb[i][d] = ['00', '00', '00'];
+                }
+            }
+            //every time is a different stars landscape
+            for(let s = 0;s<this.sts;s++){
+                let x = Math.floor(Math.random() * (this.rx-this.s));
+                let y = Math.floor(Math.random() * (this.ry-this.s));
+                this.fb[x][y] = ['11','00','AA'];
+                if(s == this.sts-1) res()
             }
         }
-        //every time is a different stars landscape
-        for(let s = 0;s<this.sts;s++){
-            let x = Math.floor(Math.random() * (this.rx-this.s));
-            let y = Math.floor(Math.random() * (this.ry-this.s));
-            this.fb[x][y] = ['11','00','AA'];       
+        else
+        {
+            res();
         }
-    }
+    })
     
 }
 
@@ -47,24 +54,27 @@ bo.d_fn = function(){
         cy = 0,
         x_l = rx,
         y_l = ry;
-    for(let i = this.mx,l = 0;l<rx;l++){
-        for(let d = this.my,ll=0;ll<ry;ll++){
-            if(this.fb[i][d][0] !== '00'){
-                c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
-                c.fillRect(cx,cy,o,o);
+    return new Promise(function(res,rej){
+        for(let i = this.mx,l = 0;l<rx;l++){
+            for(let d = this.my,ll=0;ll<ry;ll++){
+                if(this.fb[i][d][0] !== '00'){
+                    c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
+                    c.fillRect(cx,cy,o,o);
+                }
+                if((d+1)<ry)
+                    d += 1;
+                else
+                    d = 0;
+                cy += o;
+                if(l == rx-1 && ll == ry-1) res()
             }
-            if((d+1)<ry)
-                d += 1;
+            if((i+1)<rx)
+                i += 1;
             else
-                d = 0;
-            cy += o;
+                i = 0;
+            cx += o;
+            cy = 0;
         }
-        if((i+1)<rx)
-            i += 1;
-        else
-            i = 0;
-        cx += o;
-        cy = 0;
-    }
+    })
 }    
 //end bottom layer
