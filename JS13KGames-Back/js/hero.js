044e7ecac85578a7eb531d1a6a74c1a8627af179
Michele <morticcino@diegoli.org>
2019-08-22 13:49:45 +0200

    Update hero.js

--- JS13KGames-Back/js/hero.js
+++ JS13KGames-Back/js/hero.js
@@ -28,27 +28,31 @@ h.setRandomValue = function(){
 
 h.fb_fn = function(){
     this.fb = [];
-    //if I've grabbed hero,I set position of the new one
-    if(this.picked === undefined || this.picked){
-        this.setRandomValue();
-    }
-    let rx = this.rx,
-        ry = this.ry;
-    for(let i = 0;i<rx;i++){
-        this.fb[i] = [];
-        for(let d = 0;d<ry;d++){
-            //clear all
-            this.fb[i][d] = ['00', '00', '00'];
+    var me = this;
+    return new Promise(function(res,rej){
+        //if I've grabbed hero,I set position of the new one
+        if(me.picked === undefined || me.picked){
+            me.setRandomValue();
         }
-    }
-    for(let xo = 0;xo<this.s;xo++)
-    {
-        for(let yo = 0;yo<this.s;yo++)
+        let rx = me.rx,
+            ry = me.ry;
+        for(let i = 0;i<rx;i++){
+            me.fb[i] = [];
+            for(let d = 0;d<ry;d++){
+                //clear all
+                me.fb[i][d] = ['00', '00', '00'];
+            }
+        }
+        for(let xo = 0;xo<me.s;xo++)
         {
-            if(hero[0][yo][xo] === 'W')
-                this.fb[this.cx+xo][this.cy+yo] = ['11','FF','00'];
+            for(let yo = 0;yo<me.s;yo++)
+            {
+                if(hero[0][yo][xo] === 'W')
+                    me.fb[me.cx+xo][me.cy+yo] = ['11','FF','00'];
+            }
         }
-    }
+
+    })
     
     
 }
@@ -73,74 +77,77 @@ h.d_fn = function(){
         cx = 0, 
         cy = 0,
         d_o = 0,
-        reset_cy = 0;
-    //draw the sprite in fb
-    function checkQuadrant(me,x,y){
-        return (me.qux === x && me.quy === y);
-    }
-    
-    if(checkQuadrant(this,qx-1,qy) || checkQuadrant(this,qx,qy+1) || checkQuadrant(this,qx-1,qy+1) || checkQuadrant(this,qx,qy)){
-        let init_i,init_d,condition_i,condition_d = 0;
-
-        
-        init_i = sx;
-        init_d = sy;
-        condition_i = rx;
-        condition_d = ry;
-        //wrong <- ->
-        if(checkQuadrant(this,qx,qy)){
-            condition_i = rx-sx;
-            condition_d = ry-sy;
-            
-  }
-
-        //check if I'am in up quadrant
-        //wrong v ^
-        if(checkQuadrant(this,qx-1,qy)){
-            //condition_i = sx;
-            //condition_d = sy;
-            //cy = sy -1;
+        reset_cy = 0,
+        me = this;
+    return new Promise(function(res,rej){
+        //draw the sprite in fb
+        function checkQuadrant(me,x,y){
+            return (me.qux === x && me.quy === y);
         }
 
-        //check if I'am in the left
-        if(checkQuadrant(this,qx,qy+1)){
-            init_i = sx-1;
-            //init_d = ry+sy-1;
-        }
+        if(checkQuadrant(me,qx-1,qy) || checkQuadrant(me,qx,qy+1) || checkQuadrant(me,qx-1,qy+1) || checkQuadrant(me,qx,qy)){
+            let init_i,init_d,condition_i,condition_d = 0;
 
-        //check if I'am in the left up
-        
-        if(checkQuadrant(this,qx-1,qy+1)){
-            //init_i = 0;
-            //init_d = 0;
-            //cx = (rx - sx)*o;
-            //cy = (ry + sy)*o;
-            //reset_cy = (ry - sy)*o;
-            //init_i = 0;
-            //init_d = 0;
-            //cx = (rx - sx)*o;
-            //cy = (ry + sy)*o;
-            //reset_cy = (ry - sy)*o;
-            
-        }
-        for(let i = init_i;i<condition_i;i++){
-            for(let d = init_d;d<condition_d;d++){
-                if(!(this.fb[i] && this.fb[i][d] && this.fb[i][d][0]))
-                    console.log('not exist '+i+', '+d);
-                if(this.fb[i][d][0] !== '00'){
-                    this.hit(cx,cy,1,1,function(){});
-                    c.fillStyle = "#"+this.fb[i][d][0]+this.fb[i][d][1]+this.fb[i][d][2];
-                    c.fillRect(cx,cy,o,o);
-                }else{
-                    c.fillStyle = "rgba(0,255,0,0.1)";
-                    c.fillRect(cx,cy,o,o);
-                }
-                cy += o;
+
+            init_i = sx;
+            init_d = sy;
+            condition_i = rx;
+            condition_d = ry;
+            //wrong <- ->
+            if(checkQuadrant(me,qx,qy)){
+                condition_i = rx-sx;
+                condition_d = ry-sy;
+
+      }
+
+            //check if I'am in up quadrant
+            //wrong v ^
+            if(checkQuadrant(me,qx-1,qy)){
+                //condition_i = sx;
+                //condition_d = sy;
+                //cy = sy -1;
             }
-            cx += o;
-            cy = reset_cy;
-            
-        }
+
+            //check if I'am in the left
+            if(checkQuadrant(me,qx,qy+1)){
+                init_i = sx-1;
+                //init_d = ry+sy-1;
+            }
+
+            //check if I'am in the left up
+
+            if(checkQuadrant(me,qx-1,qy+1)){
+                //init_i = 0;
+                //init_d = 0;
+                //cx = (rx - sx)*o;
+                //cy = (ry + sy)*o;
+                //reset_cy = (ry - sy)*o;
+                //init_i = 0;
+                //init_d = 0;
+                //cx = (rx - sx)*o;
+                //cy = (ry + sy)*o;
+                //reset_cy = (ry - sy)*o;
+
+            }
+            for(let i = init_i;i<condition_i;i++){
+                for(let d = init_d;d<condition_d;d++){
+                    if(!(me.fb[i] && me.fb[i][d] && me.fb[i][d][0]))
+                        console.log('not exist '+i+', '+d);
+                    if(me.fb[i][d][0] !== '00'){
+                        me.hit(cx,cy,1,1,function(){});
+                        c.fillStyle = "#"+me.fb[i][d][0]+me.fb[i][d][1]+me.fb[i][d][2];
+                        c.fillRect(cx,cy,o,o);
+                    }else{
+                        c.fillStyle = "rgba(0,255,0,0.1)";
+                        c.fillRect(cx,cy,o,o);
+                    }
+                    cy += o;
+                }
+                cx += o;
+                cy = reset_cy;
+
+            }
+        })
     }
 }    
 //end text layer
