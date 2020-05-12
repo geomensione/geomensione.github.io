c44c3464a5ab5175f7037b5d681a95285421e758
Michele <morticcino@diegoli.org>
2020-05-12 16:42:37 +0200

    Create f3d-button.vue

---
+++ vue/components/f3d-button.vue
@@ -0,0 +1,12 @@
+Vue.component('f3d-button', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['button'],
+    template: '<div :id="button.id" class="barButton" @click="down">{{button.fn}}</div>',
+	  methods:{
+	  	down: function(){
+			this.button.fn  = (this.button.fn.indexOf(this.button.fn1)==-1)?this.button.fn1:this.button.fn2; 
+		}
+	  }
+  })
