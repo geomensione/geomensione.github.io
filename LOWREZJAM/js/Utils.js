03d3764c66f115f137958ab344448da04dbe922a
Michele <morticcino@diegoli.org>
2020-08-03 11:11:08 +0200

    Update Utils.js

--- LOWREZJAM/js/Utils.js
+++ LOWREZJAM/js/Utils.js
@@ -1,1 +1,26 @@
-var Utils = {};
+var Utils = {
+  c:{},
+  cx:{},
+  init3dCanvas: function(){
+
+  },
+  init2dCanvas: function(rx,ry,square = true){
+    this.c = document.createElement('canvas');
+    if(square){ 
+      let size = (window.innerWidth>window.innerHeight)?window.innerHeight:window.innerWidth;
+      this.c.width = size;
+      this.c.height = size;
+    }else{
+      this.c.width = window.innerWidth;
+      this.c.height = window.innerHeight;
+    }
+  },
+  get2DContext: function(){
+    this.cx = c.getContext('2d');
+  }
+  initLowRezCanvas: function(resx,resy){
+    this.c = document.getElementsByTagName('canvas');
+    if(this.c.length > 0) this.get2DCotext();
+    else this.init2DCanvas(resx,resy)
+  }
+};
