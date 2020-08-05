6567ce2ddc9e49db20ada1636ddf45cc8c545fb3
Michele <morticcino@diegoli.org>
2020-08-05 11:27:48 +0200

    Update hero.js

--- LOWREZJAM/assets/hero.js
+++ LOWREZJAM/assets/hero.js
@@ -1,18 +1,42 @@
- var hero = {asset:[[1,1,1,1,1,1,1,0],
-                    [0,0,0,1,0,0,0,0],
-                    [0,0,0,1,0,1,1,0],
-                    [0,0,0,1,1,1,0,0],
-                    [0,0,0,1,1,1,1,0],
-                    [0,0,0,1,0,1,0,0],
-                    [0,0,0,1,1,1,1,0],
-                    [0,0,0,1,0,1,1,0],
-                    [0,0,0,1,0,0,1,0],
-                    [0,0,0,1,1,1,1,0],
-                    [0,0,0,0,1,1,0,0],
-                    [0,0,0,0,1,1,0,0],
-                    [0,0,0,0,1,1,0,0],
-                    [0,0,0,0,1,1,0,0],
-                    [0,0,0,0,1,1,1,0]],
-             pos:{x:0,y:0}};
+ var classHero = class{
+             classHero(g){
+               this.asset = [[1,1,1,1,1,1,1,0],
+                      [0,0,0,1,0,0,0,0],
+                      [0,0,0,1,0,1,1,0],
+                      [0,0,0,1,1,1,0,0],
+                      [0,0,0,1,1,1,1,0],
+                      [0,0,0,1,0,1,0,0],
+                      [0,0,0,1,1,1,1,0],
+                      [0,0,0,1,0,1,1,0],
+                      [0,0,0,1,0,0,1,0],
+                      [0,0,0,1,1,1,1,0],
+                      [0,0,0,0,1,1,0,0],
+                      [0,0,0,0,1,1,0,0],
+                      [0,0,0,0,1,1,0,0],
+                      [0,0,0,0,1,1,0,0],
+                      [0,0,0,0,1,1,1,0]];
+               this.pos = {x:0,y:0};
+             } 
+             draw(){
+               let b = '#000000';
+               let w = '#FFFFFF';
+               let xPos = this.pos.x;
+               let yPos = this.pos.y;
+               g.cx.fillStyle = '#000000';
+               let dimx = this.asset.length;
+               let dimy = this.asset[0].length;
+               for(let tx = 0;tx<dimx;tx++){
+                 for(let ty = 0;ty<dimy;ty++){
+                   (this.asset[tx][ty] == 1)?g.cx.fillStyle=w:g.cx.fillStyle=b;
+                   g.cx.fillRect(xPos,yPos,g.tileWidth,g.tileHeight)
+                   xPos += g.tileWidth;
+                 }
+                 yPos += g.tileHeight;
+                 xPos = 0;
+                 //(hero[tx][hero[tx].length-1] == 1)?this.cx.fillStyle=w:this.cx.fillStyle=b;
+               }
+             }
+            
+            };
 
-export { hero };
+export { classHero };
