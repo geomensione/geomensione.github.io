bccce26e8b6f9b9a88ec8c5d221e637548476e53
Michele <morticcino@diegoli.org>
2020-08-06 11:53:20 +0200

    Update hero.js

--- LOWREZJAM/assets/hero.js
+++ LOWREZJAM/assets/hero.js
@@ -41,20 +41,31 @@
                ((this.frame+1)>this.nFrames-1)?this.frame=0:this.frame++;
                let b = '#000000';
                let w = '#FFFFFF';
-               let xPos = this.pos.x;
-               let yPos = this.pos.y;
-               this.g.cx.fillStyle = '#000000';
                let dimx = this.asset[this.frame].length;
                let dimy = this.asset[this.frame][0].length;
-               for(let tx = 0;tx<dimx;tx++){
-                 for(let ty = 0;ty<dimy;ty++){
-                   (this.asset[this.frame][tx][ty] == 1)?this.g.cx.fillStyle=w:this.g.cx.fillStyle=b;
-                   this.g.cx.fillRect(xPos,yPos,this.g.tileWidth,this.g.tileHeight)
-                   xPos += this.g.tileWidth;
-                 }
-                 yPos += this.g.tileHeight;
-                 xPos = this.pos.x;
-                 //(hero[tx][hero[tx].length-1] == 1)?this.cx.fillStyle=w:this.cx.fillStyle=b;
+               let yPos = this.pos.y;
+               if(this.dir == 'r'){
+                let xPos = this.pos.x;
+                for(let tx = 0;tx<dimx;tx++){
+                  for(let ty = 0;ty<dimy;ty++){
+                    (this.asset[this.frame][tx][ty] == 1)?this.g.cx.fillStyle=w:this.g.cx.fillStyle=b;
+                    this.g.cx.fillRect(xPos,yPos,this.g.tileWidth,this.g.tileHeight)
+                    xPos += this.g.tileWidth;
+                  }
+                  yPos += this.g.tileHeight;
+                  xPos = this.pos.x;
+                }
+               }else{
+                let xPos = this.pos.x + this.g.tileWidth * dimx;
+                for(let tx = dimx-1;tx>=0;tx--){
+                  for(let ty = 0;ty<dimy;ty++){
+                    (this.asset[this.frame][tx][ty] == 1)?this.g.cx.fillStyle=w:this.g.cx.fillStyle=b;
+                    this.g.cx.fillRect(xPos,yPos,this.g.tileWidth,this.g.tileHeight)
+                    xPos -= this.g.tileWidth;
+                  }
+                  yPos += this.g.tileHeight;
+                  xPos = this.pos.x + this.g.tileWidth * dimx;
+                }
                }
              }
              left(){
