3dfe7fd41b3fcebefa6cb2f1f880fa7760932c9c
Michele <morticcino@diegoli.org>
2020-08-11 10:38:57 +0200

    Update rock.js

--- LOWREZJAM/assets/rock.js
+++ LOWREZJAM/assets/rock.js
@@ -11,13 +11,13 @@
                let b = '#000000';
                let y = '#FFFF00';
                
-               let dimx = this.asset.length;
-               let dimy = this.asset[0].length;
+               let dimx = this.screen.length;
+               let dimy = this.screen[0].length;
                let yPos = this.pos.y;
                 let xPos = this.pos.x;
                 for(let tx = 0;tx<dimx;tx++){
                   for(let ty = 0;ty<dimy;ty++){
-                    (this.asset[this.frame][tx][ty] == 1)?this.g.cx.fillStyle=y:this.g.cx.fillStyle=b;
+                    (this.screen[this.frame][tx][ty] == 1)?this.g.cx.fillStyle=y:this.g.cx.fillStyle=b;
                     this.g.cx.fillRect(xPos,yPos,this.g.rockWidth,this.g.rockHeight)
                     xPos += this.g.rockWidth;
                   }
