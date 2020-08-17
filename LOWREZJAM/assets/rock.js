7aa3e8037871bc313b8c9618476bfb0037c47e63
Michele <morticcino@diegoli.org>
2020-08-17 09:47:26 +0200

    Update rock.js

--- LOWREZJAM/assets/rock.js
+++ LOWREZJAM/assets/rock.js
@@ -1,14 +1,15 @@
 import { classTile } from './tile.js' 
 import { classHero } from './hero.js' 
 import { classWall } from './wall.js' 
+import { classSnake } from './snake.js' 
 
 var classRock = class{
              constructor(g){
                this.g = g;
-               //1-tile,2-hero,3-wall
+               //1-tile,2-hero,3-wall,4-snake on left,5-snake on right
                this.screen = [[1,1,1,1],
                               [2,0,3,3],
-                              [1,0,3,3],
+                              [1,4,1,5],
                               [1,1,1,1]];
               this.pos = {x:0,y:0};
               this.drawEnv = true;
@@ -24,14 +25,25 @@ var classRock = class{
                  let xPos = this.pos.x;
                  for(let tx = 0;tx<dimx;tx++){
                     for(let ty = 0;ty<dimy;ty++){
-                      if(this.screen[tx][ty] == 1){
-                         this.g.g.push(new classTile(this.g,xPos,yPos))
-                      }else if(this.screen[tx][ty] == 2){
-                        this.g.heroObj = new classHero(this.g,xPos,yPos)
-                        this.g.g.push(this.g.heroObj)
-                      }else if(this.screen[tx][ty] == 3){
-                        this.g.g.push(new classWall(this.g,xPos,yPos))
+                      switch(this.screen[tx][ty]){
+                        case 1:
+                             this.g.g.push(new classTile(this.g,xPos,yPos))
+                             break
+                        case  2:
+                            this.g.heroObj = new classHero(this.g,xPos,yPos)
+                            this.g.g.push(this.g.heroObj)
+                            break
+                        case 3:
+                            this.g.g.push(new classWall(this.g,xPos,yPos))
+                            break
+                        case 4:
+                            this.g.g.push(new classSnake(this.g,xPos,yPos,'l'))
+                            break
+                        case 5:
+                            this.g.g.push(new classSnake(this.g,xPos,yPos,'r'))
+                            break
                       }
+                      
                       xPos += this.g.rockWidth;
                     }
                     yPos += this.g.rockHeight;
