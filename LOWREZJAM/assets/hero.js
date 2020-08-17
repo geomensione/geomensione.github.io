d5d4729964ac3a16770019a4a26a3a4f8f6c6cdc
Michele <morticcino@diegoli.org>
2020-08-17 17:22:14 +0200

    Update hero.js

--- LOWREZJAM/assets/hero.js
+++ LOWREZJAM/assets/hero.js
@@ -84,10 +84,10 @@
                  }else  if(me.g.g[t].name && me.g.g[t].name == 'wall'){
                     rect2 = {x: me.g.g[t].pos.x + (me.g.rockWidth/2) - (me.g.g[t].wallWidth/2), y: me.g.g[t].pos.y, width: me.g.rockWidth/2, height: me.g.rockHeight}
                  }   
-                 if (rect1.x < rect2.x + rect2.width &&
-                     rect1.x + rect1.width > rect2.x &&
-                     rect1.y < rect2.y + rect2.height &&
-                     rect1.y + rect1.height > rect2.y) {
+                 if (rect1.x <= rect2.x + rect2.width &&
+                     rect1.x + rect1.width >= rect2.x &&
+                     rect1.y <= rect2.y + rect2.height &&
+                     rect1.y + rect1.height >= rect2.y) {
                       find = true;
                   }
                   
