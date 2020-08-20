71ad6fa06ab48e87f27eccf3553c8546f98c9972
Michele <morticcino@diegoli.org>
2020-08-20 11:56:53 +0200

    Update tile.js

--- LOWREZJAM/assets/tile.js
+++ LOWREZJAM/assets/tile.js
@@ -4,6 +4,9 @@ var classTile = class{
                this.pos = {x:px,y:py};
                this.name = 'tile';
              } 
+             getBBox(){
+               return {x:this.pos.x,y:this.pos.y,width:this.g.rockWidth,height:this.g.rockHeight}
+             }
              draw(){
                let y = '#FFFF00';
                this.g.cx.fillStyle=y;
