7ccd510e765a9829bab22f406a2e96c014be62c2
Michele <morticcino@diegoli.org>
2020-08-21 16:32:48 +0200

    Update laser.js

--- LOWREZJAM/assets/laser.js
+++ LOWREZJAM/assets/laser.js
@@ -28,6 +28,7 @@ var classLaser = class{
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
                       find = true;
                       this.h.g.g[t].hide = true;
+                      this.h.g.increaseScore(50);
                  }
                 
                 }
