948ca53fe25bdbea7958c99d6c77e93e2a633db7
Michele <morticcino@diegoli.org>
2020-08-21 17:25:48 +0200

    Update laser.js

--- LOWREZJAM/assets/laser.js
+++ LOWREZJAM/assets/laser.js
@@ -22,7 +22,7 @@ var classLaser = class{
                this.h.g.cx.stroke();
               for(let t = 0,g_l = this.h.g.g.length;t<g_l;t++){
                 
-                if(this.h.g.g[t].getBBox && this.h.g.g[t].name.indexOf('snake') != -1){
+                if(this.h.g.g[t].getBBox && !this.h.g.g[t].hide && this.h.g.g[t].name.indexOf('snake') != -1){
                  var rect2 = this.h.g.g[t].getBBox();
                  
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
