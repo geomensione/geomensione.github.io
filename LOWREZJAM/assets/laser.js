a1a14ee940581bfbb2972f6abd5b9efbf6a9a9f5
Michele <morticcino@diegoli.org>
2020-08-21 15:08:55 +0200

    Update laser.js

--- LOWREZJAM/assets/laser.js
+++ LOWREZJAM/assets/laser.js
@@ -20,14 +20,14 @@ var classLaser = class{
                this.h.g.cx.strokeStyle = "blue";
                this.h.g.cx.rect(rect1.x, rect1.y, rect1.width, rect1.height);
                this.h.g.cx.stroke();
-              for(let t = 0,g_l = this.h.g.length;t<g_l;t++){
+              for(let t = 0,g_l = this.h.g.g.length;t<g_l;t++){
                 
-                if(this.h.g[t].getBBox && this.h.g[t].name.indexOf('hero') == -1){
-                 var rect2 = this.h.g[t].getBBox();
+                if(this.h.g.g[t].getBBox && this.h.g.g[t].name.indexOf('hero') == -1){
+                 var rect2 = this.h.g.g[t].getBBox();
                  
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y)
                       find = true;
-                      this.h.g.splice(t,1)
+                      this.h.g.g.splice(t,1)
                 
                 }
                 if(t==g_l-1) return find;
