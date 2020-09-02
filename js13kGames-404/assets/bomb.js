bced79aa1904ab159f6adc4d6d6e548468f0af83
Michele <morticcino@diegoli.org>
2020-09-02 18:45:23 +0200

    Update bomb.js

--- js13kGames-404/assets/bomb.js
+++ js13kGames-404/assets/bomb.js
@@ -49,6 +49,16 @@ var classBomb = class{
                  }
                 
                 }
+                
+                if(this.h.getBBox){
+                 var rect2 = this.h.getBBox();
+                 
+                 if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
+                      find = true;
+                      this.h.die();
+                 }
+                
+                }
                 if(t==g_l-1) return find;
 
                   
