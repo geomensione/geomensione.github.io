e6123196787f42b16ab70ead5d64467ddf4e9dc7
Michele <morticcino@diegoli.org>
2020-09-03 23:51:28 +0200

    fix bat

--- js13kGames-404/assets/bat.js
+++ js13kGames-404/assets/bat.js
@@ -4,13 +4,14 @@ var classBat = class{
                this.name = 'bat';
                this.asset = [1,0,0,0,1,0,1,0,1,0,0,0,1,0,0];
                this.spriteWidth = 5;
+               this.spriteHeight =  this.asset.length/this.spriteWidth;
                this.xPos = x;
                this.yPos = y;
                this.cirPoints = [];
-               this.circumferencePoints();
                this.frame = 0;
                this.nFrames = 10;
                this.radius = 30;
+               this.circumferencePoints();
              } 
              circumferencePoints(){
               for(let a = 0;a<1;){
@@ -21,20 +22,20 @@ var classBat = class{
               }
              }
              getBBox(){               
-               return {x:this.xPos,y:this.yPos,width:5*this.h.g.tileWidth,height:3*this.h.g.tileWidth}
+               return {x:this.xPos,y:this.yPos,width:this.spriteWidth*this.h.g.tileWidth,height:this.spriteHeight*this.h.g.tileWidth}
              }
              hit(){
               var find = false;
               var rect1 = this.getBBox();
                
-              for(let t = 0,g_l = this.h.g.g[this.h.rockObj.position].length;t<g_l;t++){
+              for(let t = 0,g_l = this.h.g[this.h.rockObj.position].length;t<g_l;t++){
                 
-                if(this.h.g.g[this.h.rockObj.position][t].getBBox && !this.h.g.g[this.h.rockObj.position][t].hide && this.h.g.g[this.h.rockObj.position][t].name.indexOf('snake') != -1){
-                 var rect2 = this.h.g.g[this.h.rockObj.position][t].getBBox();
+                if(this.h.g[this.h.rockObj.position][t].getBBox && !this.h.g[this.h.rockObj.position][t].hide && this.h.g[this.h.rockObj.position][t].name.indexOf('snake') != -1){
+                 var rect2 = this.h.g[this.h.rockObj.position][t].getBBox();
                  
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
                       find = true;
-                      this.h.g.g[this.h.rock.position][t].hide = true;
+                      this.h.g[this.h.rock.position][t].hide = true;
                       this.h.g.increaseScore(50);
                  }
                 
@@ -51,15 +52,15 @@ var classBat = class{
                let v = '#ff00ff';
                this.h.cx.fillStyle=v;
                let dimy = this.asset.length;
-               let yPos = this.cirPoints[this.frame].y;
-               let xPos = this.cirPoints[this.frame].x;
+               let yPos = this.cirPoints[this.frame].yp;
+               let xPos = this.cirPoints[this.frame].xp;
                 for(let ty = 0;ty<dimy;ty++){
-                  if(this.asset[ty] == 1)this.h.cx.fillRect(xPos,yPos,this.h.g.tileWidth,this.h.g.tileHeight)
+                  if(this.asset[ty] == 1)this.h.cx.fillRect(xPos,yPos,this.h.tileWidth,this.h.tileHeight)
                   if((ty+1)%this.spriteWidth == 0){
-                    yPos += this.h.g.tileHeight;
-                    yPos += this.cirPoints[this.frame].x;
+                    yPos += this.h.tileHeight;
+                    xPos = this.cirPoints[this.frame].xp;
                   }else{
-                    yPos += this.h.g.tileWidth;
+                    xPos += this.h.tileWidth;
                   }
                 }
                 
