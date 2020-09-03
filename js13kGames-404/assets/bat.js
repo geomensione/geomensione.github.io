d6439327def0c3e7f8be191021bbb0dd6d7c55b8
Michele <morticcino@diegoli.org>
2020-09-03 19:26:56 +0200

    Update bat.js

--- js13kGames-404/assets/bat.js
+++ js13kGames-404/assets/bat.js
@@ -27,10 +27,10 @@ var classBat = class{
               var find = false;
               var rect1 = this.getBBox();
                
-              for(let t = 0,g_l = this.h.g.g[this.h.rock.position].length;t<g_l;t++){
+              for(let t = 0,g_l = this.h.g.g[this.h.rockObj.position].length;t<g_l;t++){
                 
-                if(this.h.g.g[this.h.rock.position][t].getBBox && !this.h.g.g[this.h.rock.position][t].hide && this.h.g.g[this.h.rock.position][t].name.indexOf('snake') != -1){
-                 var rect2 = this.h.g.g[this.h.rock.position][t].getBBox();
+                if(this.h.g.g[this.h.rockObj.position][t].getBBox && !this.h.g.g[this.h.rockObj.position][t].hide && this.h.g.g[this.h.rockObj.position][t].name.indexOf('snake') != -1){
+                 var rect2 = this.h.g.g[this.h.rockObj.position][t].getBBox();
                  
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
                       find = true;
