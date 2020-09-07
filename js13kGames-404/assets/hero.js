2e6a3c446b4f64b7185cbf6808be95b3fa8171fd
Michele <morticcino@diegoli.org>
2020-09-07 08:54:20 +0200

    Update hero.js

--- js13kGames-404/assets/hero.js
+++ js13kGames-404/assets/hero.js
@@ -92,7 +92,7 @@
                 // }   
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
                   find = true;
-                  if('snakebat'.indexOf(me.g.g[me.rock.position][t].name) != -1 ){
+                  if('snakebat'.indexOf(me.g.g[me.rock.position][t].name) != -1 && !this.hide){
                     this.hide = true;
                     this.die();
                   }
