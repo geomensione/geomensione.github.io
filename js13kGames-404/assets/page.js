6db2d35f0fd785a4339d496db20a2f3f0940aabf
Michele <morticcino@diegoli.org>
2020-09-09 17:55:11 +0200

    Update page.js

--- js13kGames-404/assets/page.js
+++ js13kGames-404/assets/page.js
@@ -58,7 +58,7 @@ var classPage = class{
                  if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
                       find = true;
                       this.hide = true;
-                      this.h.g.findPage();
+                      this.h.heroObj.g.findPage();
                  }
                 
                 }
