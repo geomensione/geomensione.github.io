8511dd7597be3765ea2cd11b59ea43e742720916
Michele <morticcino@diegoli.org>
2020-09-09 10:35:19 +0200

    Update bomb.js

--- js13kGames-404/assets/bomb.js
+++ js13kGames-404/assets/bomb.js
@@ -31,12 +31,13 @@ var classBomb = class{
               if((ts - this.TScreated)>this.timer) this.explode = true;
               var rect1 = this.getBBox();
               var find = false;
+               /*
                this.h.g.cx.beginPath();
                this.h.g.cx.lineWidth = "1"
                this.h.g.cx.strokeStyle = "blue";
                this.h.g.cx.rect(rect1.x, rect1.y, rect1.width, rect1.height);
                this.h.g.cx.stroke();
-              
+              */
               for(let t = 0,g_l = this.h.g.g[this.h.rock.position].length;t<g_l;t++){
                 
                 if(this.h.g.g[this.h.rock.position][t].getBBox && !this.h.g.g[this.h.rock.position][t].hide && this.h.g.g[this.h.rock.position][t].name.indexOf('wall') != -1){
