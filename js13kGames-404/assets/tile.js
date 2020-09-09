f0fbf37bdb114bca9151e059f26a7abb5b7e065d
Michele <morticcino@diegoli.org>
2020-09-10 00:15:59 +0200

    random rock color

--- js13kGames-404/assets/tile.js
+++ js13kGames-404/assets/tile.js
@@ -8,6 +8,8 @@ var classTile = class{
                return {x:this.pos.x,y:this.pos.y,width:this.g.rockWidth,height:this.g.rockHeight}
              }
              draw(){
+               /*let y = '#';
+               for(let i = 0,s_l=6;i<s_l;i++)y+=(Math.round(Math.random()))?'F':'0'; */
                let y = '#FFFF00';
                this.g.cx.fillStyle=y;
                this.g.cx.fillRect(this.pos.x,this.pos.y,this.g.rockWidth,this.g.rockHeight)
