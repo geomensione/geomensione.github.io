d3fb766cdbb1db42d8146a443b748a74c801aa42
Michele <morticcino@diegoli.org>
2020-09-12 12:47:55 +0200

    add zzfx and sounds

--- js13kGames-404/assets/build.js
+++ js13kGames-404/assets/build.js
@@ -1,3 +1,6 @@
+zzfxV=.3
+zzfx=(q=1,k=.05,c=220,e=0,t=0,u=.1,r=0,F=1,v=0,z=0,w=0,A=0,l=0,B=0,x=0,G=0,d=0,y=1,m=0,C=0)=>{let b=2*Math.PI,H=v*=500*b/zzfxR**2,I=(0<x?1:-1)*b/4,D=c*=(1+2*k*Math.random()-k)*b/zzfxR,Z=[],g=0,E=0,a=0,n=1,J=0,K=0,f=0,p,h;e=99+zzfxR*e;m*=zzfxR;t*=zzfxR;u*=zzfxR;d*=zzfxR;z*=500*b/zzfxR**3;x*=b/zzfxR;w*=b/zzfxR;A*=zzfxR;l=zzfxR*l|0;for(h=e+m+t+u+d|0;a<h;Z[a++]=f)++K%(100*G|0)||(f=r?1<r?2<r?3<r?Math.sin((g%b)**3):Math.max(Math.min(Math.tan(g),1),-1):1-(2*g/b%2+2)%2:1-4*Math.abs(Math.round(g/b)-g/b):Math.sin(g),f=(l?1-C+C*Math.sin(2*Math.PI*a/l):1)*(0<f?1:-1)*Math.abs(f)**F*q*zzfxV*(a<e?a/e:a<e+m?1-(a-e)/m*(1-y):a<e+m+t?y:a<h-d?(h-a-d)/u*y:0),f=d?f/2+(d>a?0:(a<h-d?1:(h-a)/d)*Z[a-d|0]/2):f),p=(c+=v+=z)*Math.sin(E*x-I),g+=p-p*B*(1-1E9*(Math.sin(a)+1)%2),E+=p-p*B*(1-1E9*(Math.sin(a)**2+1)%2),n&&++n>A&&(c+=w,D+=w,n=0),!l||++J%l||(c=D,v=H,n=n||1);q=zzfxX.createBuffer(1,h,zzfxR);q.getChannelData(0).set(Z);c=zzfxX.createBufferSource();c.buffer=q;c.connect(zzfxX.destination);c.start();return c};zzfxX=new(window.AudioContext||webkitAudioContext);zzfxR=44100
+
 var classTile = class{
     constructor(g,px,py){
       this.g = g;
@@ -47,7 +50,8 @@ var classTile = class{
         
         //if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
         if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
-           find = true;
+            zzfx(...[,,321,,.09,.17,1,1.13,,,,,,1.9,,.4,.13,.53,.05]);
+            find = true;
              this.h.g.g[this.h.rock.position][t].hide = true;
              this.h.g.increaseScore(50);
           return;
@@ -90,6 +94,7 @@ var classTile = class{
        }
       }
       this.h.g.laserTemp();
+      zzfx(...[,,600,.01,,.47,,.45,,,,,.05,,,,.16,,.08,.46]);
       this.hit();
     }
   };
@@ -210,6 +215,7 @@ var classTile = class{
                this.g.die();
                //if(this.dir == 'r') this.pos.x -=50;
                //else if(this.dir == 'l') this.pos.x +=50;
+               zzfx(...[,,558,.04,,.08,3,.1,,-24,-824,.06,,,,,.33,,.04]); // Blip 168
                this.g.addText(rect1.x,rect1.y+rect1.height/2-40,"D I E!")
   
               }
@@ -444,6 +450,7 @@ var classTile = class{
         var rect2 = this.h.g[this.h.rockObj.position][t].getBBox();
         
         if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
+            zzfx(...[,,321,,.09,.17,1,1.13,,,,,,1.9,,.4,.13,.53,.05]); // Hit
              find = true;
              this.h.g[this.h.rock.position][t].hide = true;
              this.h.g.increaseScore(50);
@@ -548,6 +555,7 @@ var classTile = class{
         if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y){
              find = true;
              this.hide = true;
+             zzfx(...[,,420,.03,,.06,1,.01,,78,,,,,,,.21,,.03]);
              this.h.heroObj.g.findPage();
              return;
         }
@@ -1043,7 +1051,10 @@ var classTile = class{
     }
     hit(){
      let ts = Date.now()
-     if((ts - this.TScreated)>this.timer) this.explode = true;
+     if((ts - this.TScreated)>this.timer){
+        this.explode = true;
+        zzfx(...[,,106,,.11,.82,4,.94,.4,,,,,.8,,.4,.27,.84,.08]);
+     }
      var rect1 = this.getBBox();
      var find = false;
       /*
