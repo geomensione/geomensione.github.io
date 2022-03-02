8b60f79d2e8da9bba768e24c2cbb88753878f3bc
Michele <morticcino@diegoli.org>
2022-03-02 12:59:58 +0100

    Update script.js

--- bezierjs/script.js
+++ bezierjs/script.js
@@ -626,7 +626,7 @@ document.addEventListener("keydown", function (evt) {
 			  break;
 			case 'i':
 				//https://stackoverflow.com/questions/10673122/how-to-save-canvas-as-an-image-with-canvas-todataurl
-				var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  
+				var image = canvas.toDataURL("image/png")
 
 
 				window.location.href=image; 
