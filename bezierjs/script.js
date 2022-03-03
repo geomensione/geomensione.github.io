fbaf72c51c7522c46833e59eab3e8c147dd16fb4
Michele <morticcino@diegoli.org>
2022-03-03 12:11:13 +0100

    Update script.js

--- bezierjs/script.js
+++ bezierjs/script.js
@@ -154,7 +154,7 @@ var worker = new Worker("./js/offscreencanvas.js");
 
 
 handleInteraction(canvas).onupdate = (evt) => {
-	worker.postMessage({canvas: offscreen}, [offscreen]);
+	worker.postMessage({canvas: offscreen}, [offscreen,curves]);
 };
 
 document.addEventListener("keydown", function (evt) {
@@ -289,7 +289,7 @@ document.addEventListener("keydown", function (evt) {
 		evt.preventDefault();
 	}
 	if(aggiornare){
-			worker.postMessage({canvas: offscreen}, [offscreen]);
+			worker.postMessage({canvas: offscreen}, [offscreen,curves]);
 
 	}
 
@@ -321,7 +321,7 @@ function addBezier(canvas,x1,y1,x2,y2,x3,y3,x4,y4,color){
   curves[curves.length-1].outlinemax = 25;
   curves[curves.length-1].color = color;
   
-  worker.postMessage({canvas: offscreen}, [offscreen]);
+  worker.postMessage({canvas: offscreen}, [offscreen,curves]);
 
 }
 
