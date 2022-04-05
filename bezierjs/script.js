22b43b688f20ada7014573dc1eab89f0e35ecf4c
Michele <morticcino@diegoli.org>
2022-04-05 15:14:20 +0200

    Update script.js

--- bezierjs/script.js
+++ bezierjs/script.js
@@ -40,7 +40,7 @@ document.addEventListener("keydown", function (evt) {
 
 		switch(evt.key){
 			case '-':
-			  curves.forEach((e)=>{
+			  mybezier.curves.forEach((e)=>{
 			    if(!scaleFirst){
 					if(e.showBBoxMin){
 						if((e.outlinemin-1)>0) e.outlinemin--
@@ -57,7 +57,7 @@ document.addEventListener("keydown", function (evt) {
 			  break;
 			case '+':
 				
-			  curves.forEach((e)=>{
+			  mybezier.curves.forEach((e)=>{
 				  if(!scaleFirst){
 					if(e.showBBoxMin){
 						e.outlinemin++
@@ -79,7 +79,7 @@ document.addEventListener("keydown", function (evt) {
 			  break;
 			case 's':
 				scene = [];
-			        curves.forEach((e)=>{
+			        mybezier.curves.forEach((e)=>{
 					let curveString = {};
 					curveString.points = [];
 					e.points.forEach((p)=>{
@@ -96,15 +96,15 @@ document.addEventListener("keydown", function (evt) {
 			  break;
 			case 'l':
 				scene = JSON.parse(localStorage['F2D']);
-				curves.length = 0;
+				mybezier.curves.length = 0;
 			        
 					let pts = e.points;
-					curves.push(new Bezier(pts[0][0],pts[0][1],pts[1][0],pts[1][1],pts[2][0],pts[2][1],pts[3][0],pts[3][1]));
-					curves[curves.length-1].showbbx = e.showbbx;
-					curves[curves.length-1].mouse = e.mouse;
-					curves[curves.length-1].outlinemin = e.outlinemin;
-					curves[curves.length-1].outlinemax = e.outlinemax;
-					curves[curves.length-1].color = e.color;	
+					mybezier.curves.push(new Bezier(pts[0][0],pts[0][1],pts[1][0],pts[1][1],pts[2][0],pts[2][1],pts[3][0],pts[3][1]));
+					mybezier.curves[mybezier.curves.length-1].showbbx = e.showbbx;
+					mybezier.curves[mybezier.curves.length-1].mouse = e.mouse;
+					mybezier.curves[mybezier.curves.length-1].outlinemin = e.outlinemin;
+					mybezier.curves[mybezier.curves.length-1].outlinemax = e.outlinemax;
+					mybezier.curves[mybezier.curves.length-1].color = e.color;	
 				
 				aggiornare = true;
 			  break;
@@ -118,19 +118,19 @@ document.addEventListener("keydown", function (evt) {
 			case 'r':
 				
 				let numberOfCurves = 50;
-				curves.length = 0;
+				mybezier.curves.length = 0;
 			        for(let i = 0;i<numberOfCurves;i++){
 					let pts = [];
 					pts.push([getRandomX(),getRandomY()])
 					pts.push([getRandomX(),getRandomY()])
 					pts.push([getRandomX(),getRandomY()])
 					pts.push([getRandomX(),getRandomY()])
-					curves.push(new Bezier(pts[0][0],pts[0][1],pts[1][0],pts[1][1],pts[2][0],pts[2][1],pts[3][0],pts[3][1]));
-					curves[curves.length-1].showbbx = false;
-					curves[curves.length-1].mouse = false;
-					curves[curves.length-1].outlinemin = getRandomThick();
-					curves[curves.length-1].outlinemax = getRandomThick();
-					curves[curves.length-1].color = getRandomColor();	
+					mybezier.curves.push(new Bezier(pts[0][0],pts[0][1],pts[1][0],pts[1][1],pts[2][0],pts[2][1],pts[3][0],pts[3][1]));
+					mybezier.curves[mybezier.curves.length-1].showbbx = false;
+					mybezier.curves[mybezier.curves.length-1].mouse = false;
+					mybezier.curves[mybezier.curves.length-1].outlinemin = getRandomThick();
+					mybezier.curves[mybezier.curves.length-1].outlinemax = getRandomThick();
+					mybezier.curves[mybezier.curves.length-1].color = getRandomColor();	
 				}
 				
 				var draw = function () {
@@ -151,7 +151,7 @@ document.addEventListener("keydown", function (evt) {
 				aggiornare = true;
 			  break;
 			case 't':
-				curves.length = 0;
+				mybezier.curves.length = 0;
 				var draw = function () {
 					this.drawCurves(); //curve non crea una cubic bezier con 4 punti di controllo, ma una curva che passa nei 4 punti dati
 					this.setColor("red");
@@ -198,12 +198,12 @@ export function getRandomThick(){
 
 function addBezier(canvas,x1,y1,x2,y2,x3,y3,x4,y4,color){
   
-  curves.push(new Bezier(x1,y1,x2,y2,x3,y3,x4,y4));
-  curves[curves.length-1].showbbx = false;
-  curves[curves.length-1].mouse = false;
-  curves[curves.length-1].outlinemin = 1;
-  curves[curves.length-1].outlinemax = 25;
-  curves[curves.length-1].color = color;
+  mybezier.curves.push(new Bezier(x1,y1,x2,y2,x3,y3,x4,y4));
+  mybezier.curves[mybezier.curves.length-1].showbbx = false;
+  mybezier.curves[mybezier.curves.length-1].mouse = false;
+  mybezier.curves[mybezier.curves.length-1].outlinemin = 1;
+  mybezier.curves[mybezier.curves.length-1].outlinemax = 25;
+  mybezier.curves[mybezier.curves.length-1].color = color;
   
   ce.draw();
 
